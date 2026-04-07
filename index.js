eb3129b68d8769979605068e21d46f94145dad16
vovanty0405 <voty365@gmail.com>
2026-04-07 16:01:04 +0700

    upcode

--- index.js
+++ index.js
@@ -58,6 +58,8 @@ app.use('/chude', chudeRouter);
 app.use('/taikhoan', taikhoanRouter);
 app.use('/baiviet', baivietRouter);
 
-app.listen(3000, () => {
-	console.log('Server is running at http://127.0.0.1:3000');
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`);
 });
